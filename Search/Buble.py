ba37253e3b81b8665571fb8811b3a1c108befa30
James_Chen <yuzhecd@163.com>
2019-09-07 16:05:34 +0800

    check

--- Search/Buble.py
+++ Search/Buble.py
@@ -1,12 +1,30 @@
+from time import time
 def bubll(alist):
+    start = time()
     for i in range(len(alist)):
         for j in range(1, len(alist) - i):
             if alist[j-1] > alist[j]:
                 alist[j-1], alist[j] = alist[j], alist[j-1]
+    end = time() - start
 
-    return alist
+    return alist, end
+
+def buble_improve(alist):
+    start = time()
+    for i in range(len(alist)):
+        is_sorted = True
+        for j in range(1, len(alist)-i):
+            if alist[j-1] > alist[j]:
+                alist[j-1], alist[j] = alist[j], alist[j-1]
+                is_sorted = False
+        if is_sorted == True: break
+    end = time() - start
+    return alist, end
+
 
 if __name__ == '__main__':
     alist = [2, 1, 7, 5, 4, 10, 9]
-    r = bubll(alist)
-    print(r)
+    r, r_t= bubll(alist)
+    i, i_t = buble_improve(alist)
+    print(r, r_t)
+    print(i, i_t)
